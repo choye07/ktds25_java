7447a306ff96d6d409f0c7716591e93f1ed54a8e
최예진 <cyjbbh@gmail.com>
2025-01-23 23:29:03 +0900

    자판기 실습 문제 (scanner 이용해서 풀어보기)

--- oop/src/vendingmachine/BangBaeStation.java
+++ oop/src/vendingmachine/BangBaeStation.java
@@ -1,5 +1,7 @@
 package vendingmachine;
 
+import java.util.Scanner;
+
 public class BangBaeStation {
 	public static void main(String[]args) {
 		
@@ -11,26 +13,53 @@ public class BangBaeStation {
 		
 		VendingMachine vm = new VendingMachine(type1,type2,type3,type4);
 		
-		System.out.println("음료수 수량");
-		vm.printItem();
+//		Scanner in = new Scanner(System.in);
 		
+//		System.out.println("자판기 기능을 골라주세요.");
+//		System.out.println("1.구매하기 2.입고하기 3.종료");
+//		while(true) {
+//			int option = in.nextInt();
+			
+//			if(option ==1) {
+				System.out.println("구매할 음료수를 선택해주세요.");
+				vm.printItem();
+//				String drinkName =in.next();
+//				int orderCount = in.nextInt();
+				
+				
 		
-		String drinkName="박카스";
-		int orderCount= 2;
+				String drinkName="미에로화이바";
+				int orderCount= 2;
 		
+				
+				DrinkItem 영수증 = vm.pressItemButton(drinkName, orderCount);
+				if(영수증 != null) {
+					System.out.println(영수증.name+"를 "+orderCount+"개 샀습니다. 결제 금액은 "+영수증.price+"원 입니다.");			
+				}
+				vm.printItem();
+				
+//				System.out.println("1.구매하기 2.입고하기 3.종료");
+//			}else if(option==2) {
+//				System.out.println("입고할 제품을 고르세요");
+//				System.out.println("박카스, 몬스터, 핫식스, 밀키스");
+//				String addDrinkName =in.next();
+//				int addCount = in.nextInt();
+
+			 	String addDrinkName ="미에로화이바";
+				int addCount= 7;
+				
+				vm.addItem(addDrinkName, addCount);
+				System.out.println("음료수 수량");
+				vm.printItem();
+//				System.out.println("1.구매하기 2.입고하기 3.종료");
+//			}else {
+//				System.out.println("이용해주셔서 감사합니다.");
+//				break;
+//			}
+			
+//		}
 		
-		DrinkItem 영수증 = vm.pressItemButton(drinkName, orderCount);
-		if(영수증 != null) {
-		System.out.println(영수증.name+"를 "+orderCount+"개 샀습니다.");			
-		}
-		
-		String addDrinkName ="밀키스";
-		int addCount= 7;
-		
-		vm.addItem(addDrinkName, addCount);
-		
-		System.out.println("음료수 수량");
-		vm.printItem();
+	
 		
 		
 
--- oop/src/vendingmachine/VendingMachine.java
+++ oop/src/vendingmachine/VendingMachine.java
@@ -22,42 +22,98 @@ public class VendingMachine {
 				this.type1.stock -=orderCount;
 				//주문 내용 인스턴스 생성.
 				System.out.println("상품을 구매하였습니다.");
-				return new DrinkItem(this.type1.name,this.type1.price,orderCount);
+				return new DrinkItem(this.type1.name,(orderCount*this.type1.price),orderCount);
 			}
 		}else if(itemName==this.type2.name) {
 			if(this.type2.stock >= orderCount) {
 				this.type2.stock -=orderCount;
-				return new DrinkItem(this.type2.name,this.type2.price,orderCount);
+				return new DrinkItem(this.type2.name,(orderCount*this.type2.price),orderCount);
 			}
 		}else if(itemName==this.type3.name) {
 			if(this.type3.stock >= orderCount) {
 				this.type3.stock -=orderCount;
-				return new DrinkItem(this.type3.name,this.type3.price,orderCount);
+				return new DrinkItem(this.type3.name,(orderCount*this.type3.price),orderCount);
 			}
 		}else if(itemName==this.type4.name) {
 			if(this.type4.stock >= orderCount) {
 				this.type4.stock -=orderCount;
-				return new DrinkItem(this.type4.name,this.type4.price,orderCount);
+				return new DrinkItem(this.type4.name,(orderCount*this.type4.price),orderCount);
 			}
 		}
 		System.out.println("상품이 없습니다.");
 		return null;
+		/** scanner로 입력 받은 string은 equals를 사용해야해서
+		if(itemName.equals(this.type1.name)) {
+			//주문한 음료수의 수량이 주문 수량보다 크거나 같은지.
+			if(this.type1.stock >= orderCount) {
+				//주문한 수 만큼 수량에서 빼기
+				this.type1.stock -=orderCount;
+				//주문 내용 인스턴스 생성.
+				System.out.println("상품을 구매하였습니다.");
+				return new DrinkItem(this.type1.name,(orderCount*this.type1.price),orderCount);
+			}
+		}else if(itemName.equals(this.type2.name)) {
+			if(this.type2.stock >= orderCount) {
+				this.type2.stock -=orderCount;
+				return new DrinkItem(this.type2.name,(orderCount*this.type2.price),orderCount);
+			}
+		}else if(itemName.equals(this.type3.name)) {
+			if(this.type3.stock >= orderCount) {
+				this.type3.stock -=orderCount;
+				return new DrinkItem(this.type3.name,(orderCount*this.type3.price),orderCount);
+			}
+		}else if(itemName.equals(this.type4.name)) {
+			if(this.type4.stock >= orderCount) {
+				this.type4.stock -=orderCount;
+				return new DrinkItem(this.type4.name,(orderCount*this.type4.price),orderCount);
+			}
+		}
+		System.out.println("상품이 없습니다.");
+		return null;
+		**/
 	}
 	
 	public void addItem(String itemName, int addCount) {
 		if(itemName==this.type1.name) {
 			// 입고시키려는 음료수가 종류 중에 있다면 입고++
 			this.type1.stock +=addCount;
+			System.out.println(this.type1.name+" 상품이 "+addCount+"개 입고 되었습니다.");
 		}
 		else if(itemName==this.type2.name) {
 			this.type2.stock +=addCount;
+			System.out.println(this.type2.name+" 상품이 "+addCount+"개 입고 되었습니다.");
 		}
 		else if(itemName==this.type3.name) {
 			this.type3.stock +=addCount;
+			System.out.println(this.type3.name+" 상품이 "+addCount+"개 입고 되었습니다.");
 		}
 		else if(itemName==this.type4.name) {
 			this.type4.stock +=addCount;
+			System.out.println(this.type4.name+" 상품이 "+addCount+"개 입고 되었습니다.");
+		}else {
+			System.out.println("상품이 없어 입고 불가합니다.");			
 		}
+		/** scanner로 입력 받은 string은 equals를 사용해야해서
+		if(itemName.equals(this.type1.name)) {
+			// 입고시키려는 음료수가 종류 중에 있다면 입고++
+			this.type1.stock +=addCount;
+			System.out.println(this.type1.name+" 상품이 "+addCount+"개 입고 되었습니다.");
+		}
+		else if(itemName.equals(this.type2.name)) {
+			this.type2.stock +=addCount;
+			System.out.println(this.type2.name+" 상품이 "+addCount+"개 입고 되었습니다.");
+		}
+		else if(itemName.equals(this.type3.name)) {
+			this.type3.stock +=addCount;
+			System.out.println(this.type3.name+" 상품이 "+addCount+"개 입고 되었습니다.");
+		}
+		else if(itemName.equals(this.type4.name)) {
+			this.type4.stock +=addCount;
+			System.out.println(this.type4.name+" 상품이 "+addCount+"개 입고 되었습니다.");
+		}else {
+			System.out.println("상품이 없어 입고 불가합니다.");			
+		}
+		**/
 	}
 	public void printItem() {
 		System.out.println(this.type1.name+"의 재고는:" +this.type1.stock);
