b329ada5039163bd3ebd13cb2fe103ea07568ae6
최예진 <cyjbbh@gmail.com>
2025-02-05 23:42:01 +0900

    머지 오류

--- Task/src/task/arrayproblem/Q3.java
+++ Task/src/task/arrayproblem/Q3.java
@@ -1,9 +1,10 @@
 package task.arrayproblem;
 
-<<<<<<< .merge_file_j0IlCL
 /**
- * 아래와 같이 정수 10개로 이루어진 배열이 있습니다. [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] for 반복을 이용해 위
- * 배열에 있는 정수를 순서를 바꾸어 아래와 같이 새로운 배열로 만들어 출력해보세요. [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+ * 아래와 같이 정수 10개로 이루어진 배열이 있습니다. 
+ * [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+ * for 반복을 이용해 배열에 있는 정수를 순서를 바꾸어 아래와 같이 새로운 배열로 만들어 출력해보세요. 
+ * [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
  */
 public class Q3 {
 	public static void printArray(int[] arr) {
@@ -43,39 +44,5 @@ public class Q3 {
 		}
 		System.out.print("변경 후: ");
 		printArray(numArr);
-
-=======
-
-/**
- * 아래와 같이 정수 10개로 이루어진 배열이 있습니다.
- * [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
- * for 반복을 이용해 위 배열에 있는 정수를 순서를 바꾸어 아래와 같이 새로운 배열로 만들어 출력해보세요.
- * [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
- */
-public class Q3 {
-	public static void main(String[]args) {
-		int[] numArr = new int[10];
-		numArr[0] =1;
-		numArr[1] =2;
-		numArr[2] =3;
-		numArr[3] =4;
-		numArr[4] =5;
-		numArr[5] =6;
-		numArr[6] =7;
-		numArr[7] =8;
-		numArr[8] =9;
-		numArr[9] =10;
-		
-		int length =numArr.length;
-		
-//		for(int i=0; i<length;i++) {
-//			System.out.print("["+numArr[i]+"]");
-//		}
-		for(int i=10; i>0;i--) {
-			System.out.print("[ "+numArr[i]+"]");
-		}
-		
-		
->>>>>>> .merge_file_OXuK3V
 	}
 }
